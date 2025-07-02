cb9a990e5fa8a44a552640355ff740c640a7a18f
harshaldosh <harshal.potent@gmail.com>
2025-07-02 13:31:15 +0530

    Add PWM fading to blinky code

--- blinky/README.rst
+++ blinky/README.rst
@@ -1,21 +1,21 @@
 .. _blinky-sample:
 
-Blinky
-######
+PWM Fading Blinky
+#################
 
 Overview
 ********
 
-The Blinky sample blinks an LED forever using the :ref:`GPIO API <gpio_api>`.
+The PWM Fading Blinky sample creates a smooth fading effect on LEDs using the :ref:`PWM API <pwm_api>`.
 
 The source code shows how to:
 
-#. Get a pin specification from the :ref:`devicetree <dt-guide>` as a
-   :c:struct:`gpio_dt_spec`
-#. Configure the GPIO pin as an output
-#. Toggle the pin forever
+#. Get PWM specifications from the :ref:`devicetree <dt-guide>` as :c:struct:`pwm_dt_spec`
+#. Configure PWM channels for LED control
+#. Create smooth fading effects by varying PWM duty cycle
+#. Cycle through multiple LEDs with fading transitions
 
-See :ref:`pwm-blinky-sample` for a similar sample that uses the PWM API instead.
+This sample demonstrates advanced LED control compared to the basic GPIO blinky sample.
 
 .. _blinky-sample-requirements:
 
@@ -24,72 +24,73 @@ Requirements
 
 Your board must:
 
-#. Have an LED connected via a GPIO pin (these are called "User LEDs" on many of
+#. Have LEDs connected via PWM-capable GPIO pins (these are called "User LEDs" on many of
    Zephyr's :ref:`boards`).
-#. Have the LED configured using the ``led0`` devicetree alias.
+#. Have PWM controllers available and configured in devicetree.
+#. Support for at least one PWM channel (up to 4 LEDs supported).
 
 Building and Running
 ********************
 
-Build and flash Blinky as follows, changing ``reel_board`` for your board:
+Build and flash PWM Fading Blinky as follows, changing ``nrf5340dk_nrf5340_cpuapp`` for your board:
 
 .. zephyr-app-commands::
-   :zephyr-app: samples/basic/blinky
-   :board: reel_board
+   :zephyr-app: samples/basic/pwm_fading_blinky
+   :board: nrf5340dk_nrf5340_cpuapp
    :goals: build flash
    :compact:
 
-After flashing, the LED starts to blink. If a runtime error occurs, the sample
+After flashing, the LEDs start to fade in and out in sequence. If a runtime error occurs, the sample
 exits without printing to the console.
 
 Build errors
 ************
 
 You will see a build error at the source code line defining the ``struct
-gpio_dt_spec led`` variable if you try to build Blinky for an unsupported
+pwm_dt_spec`` variables if you try to build this sample for an unsupported
 board.
 
-On GCC-based toolchains, the error looks like this:
-
-.. code-block:: none
-
-   error: '__device_dts_ord_DT_N_ALIAS_led_P_gpios_IDX_0_PH_ORD' undeclared here (not in a function)
+The error typically indicates missing PWM controller configuration or unavailable PWM channels.
 
 Adding board support
 ********************
 
-To add support for your board, add something like this to your devicetree:
+To add support for your board, you need to configure PWM controllers and LED mappings in your devicetree:
 
-.. code-block:: DTS
+.. code-block:: devicetree
 
    / {
-   	aliases {
-   		led0 = &myled0;
-   	};
-
-   	leds {
-   		compatible = "gpio-leds";
-   		myled0: led_0 {
-   			gpios = <&gpio0 13 GPIO_ACTIVE_LOW>;
-                };
-   	};
+       pwmleds {
+           compatible = "pwm-leds";
+           
+           pwm_led0: pwm_led_0 {
+               pwms = <&pwm0 0 PWM_MSEC(1) PWM_POLARITY_INVERTED>;
+           };
+       };
+       
+       aliases {
+           pwm-led0 = &pwm_led0;
+       };
    };
 
-The above sets your board's ``led0`` alias to use pin 13 on GPIO controller
-``gpio0``. The pin flags :c:macro:`GPIO_ACTIVE_HIGH` mean the LED is on when
-the pin is set to its high state, and off when the pin is in its low state.
+   &pwm0 {
+       status = "okay";
+       pinctrl-0 = <&pwm0_default>;
+       pinctrl-names = "default";
+   };
 
 Tips:
 
-- See :dtcompatible:`gpio-leds` for more information on defining GPIO-based LEDs
+- See :dtcompatible:`pwm-leds` for more information on defining PWM-based LEDs
   in devicetree.
 
-- If you're not sure what to do, check the devicetrees for supported boards which
+- Configure pinctrl settings to map PWM channels to the correct GPIO pins.
+
+- If you're not sure what to do, check the devicetree overlays for supported boards which
   use the same SoC as your target. See :ref:`get-devicetree-outputs` for details.
 
-- See :zephyr_file:`include/zephyr/dt-bindings/gpio/gpio.h` for the flags you can use
-  in devicetree.
+- See :zephyr_file:`include/zephyr/dt-bindings/pwm/pwm.h` for PWM-specific bindings.
 
-- If the LED is built in to your board hardware, the alias should be defined in
+- If the LEDs are built in to your board hardware, the PWM configuration should be defined in
   your :ref:`BOARD.dts file <devicetree-in-out-files>`. Otherwise, you can
   define one in a :ref:`devicetree overlay <set-devicetree-overlays>`.
--- blinky/src/main.c
+++ blinky/src/main.c
@@ -5,54 +5,152 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
 #include <zephyr/sys/printk.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   500
+/* PWM period in microseconds (1kHz frequency) */
+#define PWM_PERIOD_US   1000U
 
-#define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
-#define LED2_NODE DT_ALIAS(led2)
-#define LED3_NODE DT_ALIAS(led3)
+/* Sleep time between fade steps */
+#define FADE_STEP_MS    10
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
+/* Number of fade steps */
+#define FADE_STEPS      100
+
+/* PWM device tree nodes */
+#define PWM_LED0_NODE   DT_ALIAS(pwm_led0)
+#define PWM_LED1_NODE   DT_ALIAS(pwm_led1)
+#define PWM_LED2_NODE   DT_ALIAS(pwm_led2)
+#define PWM_LED3_NODE   DT_ALIAS(pwm_led3)
+
+/* Check if PWM LED nodes exist, fallback to regular LED nodes if not */
+#if DT_NODE_EXISTS(PWM_LED0_NODE)
+static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(PWM_LED0_NODE);
+#else
+#define PWM_LED0_NODE   DT_ALIAS(led0)
+static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET_BY_IDX(PWM_LED0_NODE, 0);
+#endif
+
+#if DT_NODE_EXISTS(PWM_LED1_NODE)
+static const struct pwm_dt_spec pwm_led1 = PWM_DT_SPEC_GET(PWM_LED1_NODE);
+#else
+#define PWM_LED1_NODE   DT_ALIAS(led1)
+static const struct pwm_dt_spec pwm_led1 = PWM_DT_SPEC_GET_BY_IDX(PWM_LED1_NODE, 0);
+#endif
+
+#if DT_NODE_EXISTS(PWM_LED2_NODE)
+static const struct pwm_dt_spec pwm_led2 = PWM_DT_SPEC_GET(PWM_LED2_NODE);
+#else
+#define PWM_LED2_NODE   DT_ALIAS(led2)
+static const struct pwm_dt_spec pwm_led2 = PWM_DT_SPEC_GET_BY_IDX(PWM_LED2_NODE, 0);
+#endif
+
+#if DT_NODE_EXISTS(PWM_LED3_NODE)
+static const struct pwm_dt_spec pwm_led3 = PWM_DT_SPEC_GET(PWM_LED3_NODE);
+#else
+#define PWM_LED3_NODE   DT_ALIAS(led3)
+static const struct pwm_dt_spec pwm_led3 = PWM_DT_SPEC_GET_BY_IDX(PWM_LED3_NODE, 0);
+#endif
+
+/* Array of PWM LED specifications */
+static const struct pwm_dt_spec *pwm_leds[] = {
+    &pwm_led0,
+    &pwm_led1,
+    &pwm_led2,
+    &pwm_led3
+};
+
+#define NUM_LEDS ARRAY_SIZE(pwm_leds)
+
+/**
+ * @brief Fade LED in or out
+ * @param led_spec PWM LED specification
+ * @param fade_in true for fade in, false for fade out
  */
+static void fade_led(const struct pwm_dt_spec *led_spec, bool fade_in)
+{
+    uint32_t pulse_width;
+    
+    for (int step = 0; step <= FADE_STEPS; step++) {
+        if (fade_in) {
+            pulse_width = (PWM_PERIOD_US * step) / FADE_STEPS;
+        } else {
+            pulse_width = (PWM_PERIOD_US * (FADE_STEPS - step)) / FADE_STEPS;
+        }
+        
+        int ret = pwm_set_dt(led_spec, PWM_PERIOD_US, pulse_width);
+        if (ret < 0) {
+            printk("Error setting PWM: %d\n", ret);
+            return;
+        }
+        
+        k_msleep(FADE_STEP_MS);
+    }
+}
 
+/**
+ * @brief Set LED brightness (0-100%)
+ * @param led_spec PWM LED specification
+ * @param brightness Brightness percentage (0-100)
+ */
+static void set_led_brightness(const struct pwm_dt_spec *led_spec, uint8_t brightness)
+{
+    uint32_t pulse_width = (PWM_PERIOD_US * brightness) / 100;
+    
+    int ret = pwm_set_dt(led_spec, PWM_PERIOD_US, pulse_width);
+    if (ret < 0) {
+        printk("Error setting PWM brightness: %d\n", ret);
+    }
+}
 
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
-static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+/**
+ * @brief Turn off all LEDs
+ */
+static void turn_off_all_leds(void)
+{
+    for (int i = 0; i < NUM_LEDS; i++) {
+        pwm_set_dt(pwm_leds[i], PWM_PERIOD_US, 0);
+    }
+}
 
 int main(void)
 {
-	gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
-
-	int flag = 0;
-	int flagArray[4] = {0};
-
-	while (1) {
-		flagArray[flag] = 1;
-		gpio_pin_set_dt(&led0, flagArray[0]);
-		gpio_pin_set_dt(&led1, flagArray[1]);
-		gpio_pin_set_dt(&led3, flagArray[2]);
-		gpio_pin_set_dt(&led2, flagArray[3]);
-		flagArray[flag] = 0;
-		flag++;
-		printk("led %d activated\n", flag);
-		if(flag == 4){
-			flag = 0;
-			printk("\n");
-		}
-
-		k_msleep(SLEEP_TIME_MS);
-	}
-
-	return 0;
+    int ret;
+    
+    printk("PWM Fading Blinky Sample for nRF5340 DK\n");
+    
+    /* Check if PWM devices are ready */
+    for (int i = 0; i < NUM_LEDS; i++) {
+        if (!device_is_ready(pwm_leds[i]->dev)) {
+            printk("Error: PWM device %s is not ready\n", pwm_leds[i]->dev->name);
+            return -1;
+        }
+        printk("PWM LED %d ready\n", i);
+    }
+    
+    /* Turn off all LEDs initially */
+    turn_off_all_leds();
+    
+    int current_led = 0;
+    
+    while (1) {
+        printk("Fading LED %d\n", current_led);
+        
+        /* Fade in current LED */
+        fade_led(pwm_leds[current_led], true);
+        
+        /* Keep LED on for a moment */
+        k_msleep(200);
+        
+        /* Fade out current LED */
+        fade_led(pwm_leds[current_led], false);
+        
+        /* Move to next LED */
+        current_led = (current_led + 1) % NUM_LEDS;
+        
+        /* Small delay between LEDs */
+        k_msleep(100);
+    }
+    
+    return 0;
 }
